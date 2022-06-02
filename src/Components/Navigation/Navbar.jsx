3b6e7aa62922603a68efa3a951e7fd6b5efec125
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-02 16:20:55 -0600

    Added login button to navbar

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import IAmHereLogo from "../../images/i-am-here-logo.png";
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Toolbar, Typography } from "@mui/material";
 
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <Grid container>
-        <Grid item>
-          <img
-            src={IAmHereLogo}
-            style={{ width: 75, height: 75 }}
-            alt="application logo"
-          />
-        </Grid>
-        <Grid>
-          <Typography
-            variant="h6"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            I Am <br />
-            Here
-          </Typography>
-        </Grid>
-      </Grid>
+      <Toolbar disableGutters>
+        {/* <Grid container> */}
+        {/* <Grid item> */}
+        <img
+          src={IAmHereLogo}
+          style={{ width: 75, height: 75 }}
+          alt="application logo"
+        />
+        {/* </Grid> */}
+        {/* <Grid item> */}
+        <Typography variant="h6">
+          I Am <br />
+          Here
+        </Typography>
+        {/* </Grid> */}
+        {/* <Grid item mt={2}> */}
+          <Button variant="contained">Login</Button>
+        {/* </Grid> */}
+        {/* </Grid> */}
+      </Toolbar>
     </AppBar>
   );
 };
