599aa4d74e31baa58cc2730fee8f9ae7c8309719
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-02 11:32:14 -0600

    Added 'I am Here' name to be beside logo

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
 import AppBar from "@mui/material/AppBar";
-import IAmHereLogo from '../../images/i-am-here-logo.png'
+import IAmHereLogo from "../../images/i-am-here-logo.png";
+import { Grid, Typography } from "@mui/material";
 
 const Navbar = () => {
   return (
-      <AppBar position='sticky'>
-          <img src={IAmHereLogo} width={75} height={75} alt="application logo" />
-      </AppBar>
-  )
-}
+    <AppBar position="sticky">
+      <Grid container>
+        <Grid item>
+          <img
+            src={IAmHereLogo}
+            style={{ width: 75, height: 75 }}
+            alt="application logo"
+          />
+        </Grid>
+        <Grid>
+          <Typography
+            variant="h6"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            I Am <br />
+            Here
+          </Typography>
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
