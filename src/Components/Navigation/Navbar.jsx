f490eb6afc1336c5f23eefbe3c9d8e2813daf347
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-02 18:08:59 -0600

    *FIXED NAVBAR Login button shows up on the left in the navbar

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import IAmHereLogo from "../../images/i-am-here-logo.png";
-import { Button, Grid, Toolbar, Typography } from "@mui/material";
+import { Button, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const Navbar = () => {
   return (
     <AppBar position="sticky">
       <Toolbar disableGutters>
-        {/* <Grid container> */}
-        {/* <Grid item> */}
         <img
           src={IAmHereLogo}
           style={{ width: 75, height: 75 }}
           alt="application logo"
         />
-        {/* </Grid> */}
-        {/* <Grid item> */}
         <Typography variant="h6">
           I Am <br />
           Here
         </Typography>
-        {/* </Grid> */}
-        {/* <Grid item mt={2}> */}
+        <Box
+          sx={{
+            marginLeft: "1000px",
+          }} 
+        >
           <Button variant="contained">Login</Button>
-        {/* </Grid> */}
-        {/* </Grid> */}
+        </Box>
       </Toolbar>
     </AppBar>
   );
