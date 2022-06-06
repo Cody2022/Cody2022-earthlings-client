12f409dec52942d5bc84ee20aea637573409218f
Zia Hassan <fowziahme@gmail.com>
2022-06-06 11:18:02 -0600

    translations json added

--- src/Components/Translator/Translate.jsx
+++ src/Components/Translator/Translate.jsx
@@ -2,32 +2,53 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+const languages = [
+  {
+    code: 'fr',
+    name: 'Français',
+    country_code: 'fr',
+  },
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  },
+  {
+    code: 'ukr',
+    name: 'україни',
+    country_code: 'ua',
+  },
+]
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(HttpApi) // passes i18n down to react-i18next
+
   .init({
     // the translations
     // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      }
-    },
-    lng: "en", // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
 
-    interpolation: {
-      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    }
+    fallbackLng: "en",
+    detection: {  
+      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      caches: ['cookie'],
+  },
+  backend: {
+    loadPath: '/assets/locales//{{lng}}/translations.json',
+  },
+  react: {useSuspense: false}
   });
 
 function Translate() {
   const { t } = useTranslation();
 
-  return <h2>{t('Welcome to React')}</h2>;
+  return <h2>{t('welcome_to_earthlings')}</h2>;
 }
 
 export default Translate;
