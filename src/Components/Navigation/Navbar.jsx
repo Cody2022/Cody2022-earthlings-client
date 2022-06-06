3cbc1d89314b608afc9c6646b39c79d852a4f8c0
Zia Hassan <fowziahme@gmail.com>
2022-06-06 17:02:01 -0600

    translating login and logo working

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -3,8 +3,11 @@ import AppBar from "@mui/material/AppBar";
 import IAmHereLogo from "../../images/i-am-here-logo.png";
 import { Button, Toolbar, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useTranslation} from "react-i18next";
+
 
 const Navbar = () => {
+  const { t } = useTranslation();
   return (
     <Box>
       <AppBar position="sticky">
@@ -15,10 +18,11 @@ const Navbar = () => {
             alt="application logo"
           />
           <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-            I Am <br />
-            Here
+          {t('i_am_T')} <br />
+          {t('here_T')}
           </Typography>
-          <Button variant="contained" sx={{ mr: 10 }}>Login</Button>
+          {/* <Button variant="contained" sx={{ mr: 10 }}>LOGIN</Button> */}
+          <Button variant="contained" sx={{ mr: 10 }}>{t('login_T')}</Button>
         </Toolbar>
       </AppBar>
     </Box>
