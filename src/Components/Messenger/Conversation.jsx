0aa6cf8e684df0a6618a2a5f1728c5cf0556d094
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-26 10:23:26 -0600

    Ability to extract user info to display on page

--- src/Components/Messenger/Conversation.jsx
+++ src/Components/Messenger/Conversation.jsx
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const Conversation = ({ conversation, currentUser }) => {
-    console.log(`convo is:`, conversation)
-    // const [user, setUser] = useState(null);
+const Conversation = ({ conversation, currentUser, isLoading }) => {
+  console.log(`convo is:`, conversation);
+  const [user, setUser] = useState("");
 
-    //Get the user
-    useEffect(() => {
-         const chatMembers = conversation.members.find(member => member !== currentUser.email);
-        console.log(chatMembers);
-    },[])
+  //Get the user
+  useEffect(() => {
+    const chatMembers = conversation.members.find(
+      (member) => member !== currentUser.email
+    );
+    console.log("chatMembers are:", chatMembers);
+
+    const getUsersFirstName = async () => {
+      try {
+        const response = await axios.get("/name?email=" + chatMembers);
+        console.log(`response is:`, response);
+        setUser(response.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getUsersFirstName();
+  }, [currentUser, conversation, isLoading]);
+
+  if (isLoading) {
+    return <div>isLoading...</div>;
+  }
+
+  console.log("user is:", user);
 
   return (
-    <div>Ryan</div>
-  )
-}
+    <div>
+      {user.firstName} {user.lastName} - {user.email}
+    </div>
+  );
+};
 
 export default Conversation
