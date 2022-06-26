ecc3af2fae09ba00a2886f47ee56981b97106be5
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-26 14:05:59 -0600

    Created chatroom component

---
+++ src/Components/Messenger/ChatRoom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatRoom = () => {
+  return (
+    <div>
+      <span style={{ display: "flex", justifyContent: "center" }}>
+        You are now in a chatroom
+      </span>
+    </div>
+  );
+}
+
+export default ChatRoom
--- src/Components/Pages/ChatPage.js
+++ src/Components/Pages/ChatPage.js
@@ -1,11 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ChatRoom from "../Messenger/ChatRoom";
 import Conversation from "../Messenger/Conversation";
 
 const ChatPage = () => {
   const { user, isLoading } = useAuth0();
   const [conversationList, setConversationList] = useState([]);
+  const [currentChat, setCurrentChat] = useState(false);
+  const [messageList, setMessageList] = useState([]);
 
   useEffect(() => {
     const getConversations = async () => {
@@ -24,16 +27,36 @@ const ChatPage = () => {
 
   if (isLoading) {
     return <div>isLoading...</div>;
-  };
+  }
+
+  console.log(`Current chat is:`, currentChat)
 
   return (
     <div>
       <h1>This is the official chat messenger page. Coming Soon!!</h1>
       <p>List of conversations:</p>
-      <br />
-      {conversationList.map((c) => (
-        <Conversation conversation={c} currentUser={user} isLoading={isLoading} />
-      ))}
+      <div>
+        <input placeholder="Search for friends" />
+        {conversationList.map((c) => (
+          <div onClick={() => setCurrentChat(c)}>
+          <Conversation
+            conversation={c}
+            currentUser={user}
+            isLoading={isLoading}
+          />
+            </div>
+        ))}
+      </div>
+      <div>
+        {
+          currentChat ? (
+            <div>
+              <ChatRoom />
+            </div>
+          ) : (
+            <span style={{display: 'flex', justifyContent: 'center'}}>Open a conversation to start a chat</span>
+          )}
+      </div>
     </div>
   );
 };
