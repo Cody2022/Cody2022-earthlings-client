689f08444173191838a04b7c79a63cb75b938865
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-27 12:13:29 -0600

    removed 'console.log' to clean up console

--- src/Components/Messenger/Conversation.jsx
+++ src/Components/Messenger/Conversation.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const Conversation = ({ conversation, currentUser, isLoading }) => {
-  console.log(`convo is:`, conversation);
   const [user, setUser] = useState("");
 
   //Get the user
@@ -10,12 +9,10 @@ const Conversation = ({ conversation, currentUser, isLoading }) => {
     const chatMembers = conversation.members.find(
       (member) => member !== currentUser.email
     );
-    console.log("chatMembers are:", chatMembers);
 
     const getUsersFirstName = async () => {
       try {
         const response = await axios.get("/name?email=" + chatMembers);
-        console.log(`response is:`, response);
         setUser(response.data);
       } catch (err) {
         console.log(err.message);
@@ -27,8 +24,6 @@ const Conversation = ({ conversation, currentUser, isLoading }) => {
   if (isLoading) {
     return <div>isLoading...</div>;
   }
-
-  console.log("user is:", user);
 
   return (
     <div>
