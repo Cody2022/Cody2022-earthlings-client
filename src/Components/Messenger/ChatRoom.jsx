b55f52e97ec2e89e01584a7ae34f415c24fd103e
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-07-03 09:29:18 -0600

    Function that shows different color based on user

--- src/Components/Messenger/ChatRoom.jsx
+++ src/Components/Messenger/ChatRoom.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
-const ChatRoom = ({chatText, own}) => {
+const ChatRoom = ({ chatText, own }) => {
   return (
-    <div>
-      <span style={{ display: "flex", justifyContent: "center" }}>
-        <p>{chatText.sender}: {chatText.message}</p>
-      </span>
-    </div>
+    <Box>
+      <div>
+        <span>{chatText.sender}: </span>
+        {own ? (
+          <Typography
+            variant="p"
+            sx={{
+              padding: 0.5,
+              borderRadius: 20,
+              backgroundColor: "#0A4289",
+              color: "white",
+            }}
+          >
+            {chatText.message}
+          </Typography>
+        ) : (
+            <Typography
+            variant="p"
+              sx={{
+              padding: 0.5,
+              borderRadius: 20,
+              backgroundColor: "#c9d3f2",
+                color: "black",
+            }}
+          >
+            {chatText.message}
+          </Typography>
+        )}
+      </div>
+    </Box>
   );
-}
+};
 
-export default ChatRoom
+export default ChatRoom;
