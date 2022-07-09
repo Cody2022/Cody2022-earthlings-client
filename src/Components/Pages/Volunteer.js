2ee8a1039dac4174ee4b8dcc8fbc350eb969d0ae
Cody2022 <cody.zheng@hotmail.com>
2022-07-09 02:11:32 -0600

    Cody_Updated Volunteer page

--- src/Components/Pages/Volunteer.js
+++ src/Components/Pages/Volunteer.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from "../Loading/Loading";
+import VolunTransportForm from '../Form/VolunteerTransportForm';
+import VolunteerTransportList from '../Form/VolunteerTransportList';
+import { Container } from '@mui/system';
 
 export const Volunteer = () => {
+     const [rerender, setRerender]=useState(false);
+     const {user}=useAuth0();
+     const email=user.email;
+     const [transportList, setTransportList]=useState();
+
+     useEffect(() => {
+         const fetchTransportListByEmail = async (email) => {
+         let response = await fetch(`/transport/get/${email}`);
+         let transportList = await response.json();
+         return transportList;
+       };
+       const getTransportList = async (email) => {
+         const fetchedTransportList = await fetchTransportListByEmail(email);
+         setTransportList(fetchedTransportList);
+       };
+       getTransportList(email);
+     }, [email, rerender]);
+
+
   return (
-    <div>Volunteer</div>
+    <div style={{background: "rgba(221, 238, 137, 0.6)"}}>
+      <Container sytle={{marginBottom:5}}>
+        {transportList && <VolunteerTransportList transportList={transportList} rerender={rerender} setRerender={setRerender}/>}
+        <VolunTransportForm rerender={rerender} setRerender={setRerender} />
+       </Container>
+    </div>
   )
 }
 
