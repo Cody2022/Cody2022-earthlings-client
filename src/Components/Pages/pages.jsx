5159a18cac24723baf1a44f2ce58d17f2ed8a474
Cody2022 <cody.zheng@hotmail.com>
2022-07-14 01:36:24 -0600

    Cody_Added new page newcomertransport

--- src/Components/Pages/pages.jsx
+++ src/Components/Pages/pages.jsx
@@ -4,3 +4,4 @@ export {default as Enter} from "./Enter";
 export {default as Newcomer} from "./Newcomer";
 export {default as Volunteer} from "./Volunteer";
 export {default as Profile} from "./Profile";
+export {default as NewcomerTransport} from "./NewcomerTransport"
