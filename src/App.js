826d89ac6e65a7105cd3adbb8dfa120379febc87
Cody2022 <cody.zheng@hotmail.com>
2022-07-19 00:22:33 -0600

    fixed the error at App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import volunteersProfile from "./Components/Volunteers/volunteersProfile";
 import Testing from "./Components/Volunteers/Testing";
 import BigCalendarTest from "./Components/Calendar/BigCalendarTest";
 import VolunTransportForm from "./Components/Form/VolunteerTransportForm";
-import TranslationSubmit from "./Components/Form/NewComersForms.jsx/TranslationSubmit";
 
 function App() {
   return (
@@ -35,8 +34,6 @@ function App() {
         <Route path="/accommodation" element={<AccommodationForm />} />
         <Route path="/testProfile" element={<Testing />} />
         <Route path="/transport" element={<VolunTransportForm />} />
-        <Route path="/NewForm" element={<TranslationSubmit />} />
-        
       </Routes>
 
       <Footer />
