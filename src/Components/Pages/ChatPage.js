1325fdd5b7f4053aaea214fbce1237d6cc4dbd5a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-07-20 17:29:23 -0600

    style - Input field at bottom of page

--- src/Components/Pages/ChatPage.js
+++ src/Components/Pages/ChatPage.js
@@ -116,8 +116,7 @@ const ChatPage = () => {
       display: 'flex',
       padding: 1
     }}>
-      <Paper elevation={24} sx={{
-      }}>
+      <Paper elevation={24}>
         <Typography variant="h6" gutterBottom={true} sx={{
           padding: 1
         }}>My Connections</Typography>
@@ -132,8 +131,15 @@ const ChatPage = () => {
         ))}
       </Paper>
       <Paper elevation={24} sx={{
-        width: 1
+        display: 'flex',
+        flexDirection: 'column',
+        width: 1,
+        height: '70vh'
       }}>
+          <Box style={{
+          flexGrow: 1,
+            overflow: 'auto'
+        }}>
         {currentChat ? (
           <Grid>
             {messageList.map((m) => (
@@ -146,11 +152,12 @@ const ChatPage = () => {
             <Typography variant='h5'>
             Open a conversation to start a chat
           </Typography>
-        )}
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+            )}
+            </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderTop: 'double'}} >
           <Input
             sx={{
-              width: '80%'
+              width: '80%',
             }}
           placeholder="Write Something"
           onChange={(e) => setNewMessage(e.target.value)}
