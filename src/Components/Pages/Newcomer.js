1d8913aa5685d5287eb49a5ea1c94ebb1d0bb7c3
Cody2022 <cody.zheng@hotmail.com>
2022-07-21 00:24:07 -0600

    Cody_modified newcomer landing page

--- src/Components/Pages/Newcomer.js
+++ src/Components/Pages/Newcomer.js
@@ -3,8 +3,6 @@ import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from "../Loading/Loading";
 import { Categories } from '../Category/Categories';
 import { Box } from '@mui/system';
-import { Grid, Typography } from '@mui/material';
-import AccommodationList from '../AccommodationList';
 
 
 export const Newcomer = () => {
@@ -12,13 +10,7 @@ export const Newcomer = () => {
   return (
     <Box
      style={{background:"#10e1eb54", backgroundSize: "cover"}}>
-      <Grid>
-        Newcomer
-      </Grid>
       <Categories />
-       
-      {/* <Typography variant='h6' mx={3}>Available Accommodations</Typography> */}
-      {/* <AccommodationList /> */}
     </Box>
   )
 }
