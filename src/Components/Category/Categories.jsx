b49d94488645eb89788ad9c1dc1dcb257d93ec1d
Cody2022 <cody.zheng@hotmail.com>
2022-07-21 00:21:33 -0600

    Cody_modifed Categories component

--- src/Components/Category/Categories.jsx
+++ src/Components/Category/Categories.jsx
@@ -1,4 +1,4 @@
-import { Container, Divider, Grid } from '@mui/material'
+import { Container, Divider, Grid, Typography } from '@mui/material'
 import React from 'react';
 import TransportCard from './TransportCard';
 import TranslationCard from './TranslationCard';
@@ -10,31 +10,66 @@ import  BrowseCategories from './BrowseCategories';
 
 export const Categories = () => {
   return (
-    <Container>
+    <Container sx={{ pt: 3 }}>
       <BrowseCategories />
-      <Divider>CENTER</Divider>
-      <Grid container spacing={2} display="flex" justifyContent="center" >
+      <Divider sx={{ p: 2 }}>
+        <Typography color="CaptionText" fontWeight="bold"
+          fontSize="1.5rem">
+          Find Services
+        </Typography>
+      </Divider>
+      <Grid container spacing={2} display="flex" justifyContent="center">
         <Grid item display="flex" justifyContent="center" xs={12} sm={6} md={3}>
           <TransportCard />
         </Grid>
 
-        <Grid item display="flex" justifyContent="center" xs={12} sm={6} md={2.9} >
+        <Grid
+          item
+          display="flex"
+          justifyContent="center"
+          xs={12}
+          sm={6}
+          md={2.9}
+        >
           <TranslationCard />
         </Grid>
 
-        <Grid item display="flex" justifyContent="center" xs={12} sm={6} md={3.2}>
+        <Grid
+          item
+          display="flex"
+          justifyContent="center"
+          xs={12}
+          sm={6}
+          md={3.2}
+        >
           <AccommodationCard />
         </Grid>
 
-        <Grid item display="flex" justifyContent="center" xs={12} sm={6} md={2.9}>
+        <Grid
+          item
+          display="flex"
+          justifyContent="center"
+          xs={12}
+          sm={6}
+          md={2.9}
+        >
           <EducationCard />
         </Grid>
       </Grid>
-      <br/>
-      <Container style={{display:"flex", justifyContent:"center", marginTop: 15, marginBottom: 20, fontWeight:"bold", fontSize: '1.5rem' }}>Featured Volunteers</Container>
+      <br />
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 15,
+          marginBottom: 20,
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+        }}
+      >
+        Featured Volunteers
+      </Container>
       <FeaturedVolunteer />
-    
-      <Container style={{display:"flex", justifyContent:"center", paddingTop: 50, paddingBottom: 40, fontWeight:"bold", fontSize: '1.5rem' }}>Placeholder</Container>
     </Container>
-  )
+  );
 }
